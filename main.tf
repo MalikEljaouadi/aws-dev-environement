c941b9c653f06a1cb2ddf4bc4b8525332ad2893c
Malik Eljaouadi <malekabidi25@gmail.com>
2024-07-03 16:27:52 +0200

    ✨ Add the internet gateway

--- main.tf
+++ main.tf
@@ -9,12 +9,20 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_subnet" "main_public_subnet" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.123.1.0/24" # This is one of the subnets within 10.123.0.0/16
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.123.1.0/24" # This is one of the subnets within 10.123.0.0/16
   map_public_ip_on_launch = true
-  availability_zone = "eu-central-1a"
+  availability_zone       = "eu-central-1a"
 
   tags = {
     Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "main_internet_gateway" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "dev-igw"
   }
 }
