4c97070d7e9ea76400ca6420b54a32ac9ce14af5
Malik Eljaouadi <malekabidi25@gmail.com>
2024-07-03 16:58:00 +0200

    🧱 define the association between the subnet and the internet gateway

--- main.tf
+++ main.tf
@@ -36,7 +36,12 @@ resource "aws_route_table" "main_route_table" {
 }
 
 resource "aws_route" "default_route" {
-  route_table_id            = aws_route_table.main_route_table.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main_internet_gateway.id
+  route_table_id         = aws_route_table.main_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_internet_gateway.id
+}
+
+resource "aws_route_table_association" "main_association" {
+  subnet_id      = aws_subnet.main_public_subnet.id
+  route_table_id = aws_route_table.main_route_table.id
 }
