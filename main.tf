f04414dc269f6a59d3c2812fff5b53779d50f36c
Malik Eljaouadi <malekabidi25@gmail.com>
2024-07-03 16:19:40 +0200

    ✨ Add the subnet

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
-resource "aws_vpc" "main-vpc" {
-  cidr_block           = "10.123.0.0/18"
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.123.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
     Name = "dev"
+  }
+}
+
+resource "aws_subnet" "main_public_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.123.1.0/24" # This is one of the subnets within 10.123.0.0/16
+  map_public_ip_on_launch = true
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "dev-public"
   }
 }
